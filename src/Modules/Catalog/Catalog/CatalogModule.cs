10236d0a2f8ba5179f9cd66d4d0b1f2627e0f231
Berkay Kulak <kulakberkay15@gmail.com>
2024-10-24 02:16:05 +0300

    Develop Catalog API Endpoints Expose Minimal Api Endpoints with Carter and REPR
    Develop Cross-cutting Concerns for Catalog and Each Module in EShop Modules

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore.Diagnostics;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Behaviors;
 using Shared.Data.Interceptors;
 
 
@@ -15,7 +15,11 @@ public static class CatalogModule
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         var connectionString = configuration.GetConnectionString("Database");
 
